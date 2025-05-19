0348405930ded3d4e504f79717beb3896af137af
HeeCheol Shin <crispin@si-analytics.ai>
2025-05-19 14:41:18 +0900

    :sparkles: Feat: 공연 도메인 필드 값 추가

--- src/main/kotlin/com/crispinlab/cookieshow/application/domain/Performance.kt
+++ src/main/kotlin/com/crispinlab/cookieshow/application/domain/Performance.kt
@@ -1,8 +1,14 @@
 package com.crispinlab.cookieshow.application.domain
 
-internal data class Performance(
+import java.time.Instant
+
+data class Performance(
     val id: Long? = null,
     val title: String,
     val description: String,
-    val venue: Long
+    val venue: Long,
+    val startTime: Instant,
+    val endTime: Instant,
+    val reservationStartTime: Instant,
+    val reservationEndTime: Instant
 )
--- src/main/kotlin/com/crispinlab/cookieshow/application/service/extensions/ServiceExtensions.kt
+++ src/main/kotlin/com/crispinlab/cookieshow/application/service/extensions/ServiceExtensions.kt
@@ -1,11 +1,59 @@
 package com.crispinlab.cookieshow.application.service.extensions
 
 import com.crispinlab.cookieshow.application.domain.Performance
+import com.crispinlab.cookieshow.application.usecase.PerformanceRegisterUseCase
 import com.crispinlab.cookieshow.repository.entity.PerformanceEntity
+import java.time.Duration
+import java.time.Instant
+import java.time.ZoneId
+
+internal fun PerformanceRegisterUseCase.RegisterRequest.toDomain() =
+    Performance(
+        title = this.title,
+        description = this.description,
+        venue = this.venue,
+        startTime = this.startTime,
+        endTime = this.endTime,
+        reservationStartTime =
+            this.reservationStartTime
+                ?: defaultReservationStartTime(this.startTime),
+        reservationEndTime =
+            this.reservationEndTime
+                ?: defaultReservationEndTime(this.startTime)
+    )
 
 internal fun Performance.toEntity(): PerformanceEntity =
     PerformanceEntity(
         title = this.title,
         description = this.description,
-        venue = this.venue
+        venue = this.venue,
+        startTime = this.startTime,
+        endTime = this.endTime,
+        reservationStartTime = this.reservationStartTime,
+        reservationEndTime = this.reservationEndTime
     )
+
+internal fun Performance.toDto(id: Long): PerformanceRegisterUseCase.RegisterResponse =
+    PerformanceRegisterUseCase.RegisterResponse(
+        id = id,
+        title = this.title,
+        description = this.description,
+        venue = this.venue,
+        startTime = this.startTime,
+        endTime = this.endTime,
+        reservationStartTime = this.reservationStartTime,
+        reservationEndTime = this.reservationEndTime
+    )
+
+private fun defaultReservationStartTime(startTime: Instant): Instant =
+    startTime
+        .atZone(ZoneId.of("Asia/Seoul"))
+        .run {
+            toLocalDate()
+                .minusDays(7)
+                .atStartOfDay(ZoneId.of("Asia/Seoul"))
+                .toInstant()
+        }
+
+private fun defaultReservationEndTime(startTime: Instant): Instant =
+    startTime.minus(Duration.ofHours(1))
