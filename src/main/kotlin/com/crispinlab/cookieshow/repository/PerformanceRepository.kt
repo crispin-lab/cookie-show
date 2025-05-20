d8ad7a77abd65b218487c31c369704b052172564
crispin <h.c.shin.dev09@gmail.com>
2025-05-20 22:50:07 +0900

    :sparkles: Feat: 공연 리스트 조회 기능 추가

--- src/main/kotlin/com/crispinlab/cookieshow/repository/PerformanceRepository.kt
+++ src/main/kotlin/com/crispinlab/cookieshow/repository/PerformanceRepository.kt
@@ -2,7 +2,44 @@ package com.crispinlab.cookieshow.repository
 
 import com.crispinlab.cookieshow.repository.entity.PerformanceEntity
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-internal interface PerformanceRepository : JpaRepository<PerformanceEntity, Long>
+internal interface PerformanceRepository : JpaRepository<PerformanceEntity, Long> {
+    @Query(
+        value =
+            """
+            SELECT COUNT(*)
+            FROM (
+                SELECT performances.id
+                FROM performances
+                LIMIT :limit
+            ) t
+            """,
+        nativeQuery = true
+    )
+    fun count(
+        @Param("limit") limit: Long
+    ): Long
+
+    @Query(
+        value =
+            """
+            SELECT performances.id, performances.title, performances.description, performances.venue, performances.start_time, performances.end_time,
+                performances.reservation_start_time, performances.reservation_end_time
+            FROM (
+                SELECT performances.id
+                FROM performances
+                OFFSET :offset LIMIT :limit
+            ) t
+            LEFT JOIN performances ON t.id = performances.id
+            """,
+        nativeQuery = true
+    )
+    fun findAll(
+        @Param("offset") offset: Long,
+        @Param("limit") limit: Long
+    ): List<PerformanceEntity>
+}
