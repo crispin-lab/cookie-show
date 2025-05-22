7ef7824437d17586b536d198724ab240002e1d75
HeeCheol Shin <crispin@si-analytics.ai>
2025-05-22 16:20:52 +0900

    :sparkles: Feat: 공연 등록 컨트롤러 추가

--- src/main/kotlin/com/crispinlab/cookieshow/application/usecase/PerformanceRegisterUseCase.kt
+++ src/main/kotlin/com/crispinlab/cookieshow/application/usecase/PerformanceRegisterUseCase.kt
@@ -20,8 +20,8 @@ internal interface PerformanceRegisterUseCase {
         val venue: Long,
         val startTime: Instant,
         val endTime: Instant,
-        val reservationStartTime: Instant?,
-        val reservationEndTime: Instant?
+        val reservationStartTime: Instant,
+        val reservationEndTime: Instant
     )
 
     fun register(request: RegisterRequest): RegisterResponse
